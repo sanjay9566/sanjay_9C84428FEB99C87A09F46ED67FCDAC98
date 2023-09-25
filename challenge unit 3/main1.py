773408bb4db9641342a864af0e196316a8713daf
sanjay9566 <144333143+sanjay9566@users.noreply.github.com>
2023-09-25 13:54:58 +0530

    Update main1.py

--- challenge unit 3/main1.py
+++ challenge unit 3/main1.py
@@ -4,8 +4,8 @@ def linear_search_product(product_list, target_product):
         if product == target_product:
             indices.append(i)
     return indices
-products = ["mango ", "Banana", "Orange", "Grapes", "Apple", "Grapes","Apple"]
-target = "mango "
+products = ["Apple ", "Banana", "Orange", "Grapes", "Apple", "Grapes","Apple"]
+target = "Apple"
 result = linear_search_product(products, target)
 if result:
     print(f"The product '{target}' was found at indices: {result}")
